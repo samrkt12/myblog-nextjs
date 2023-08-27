8e5f3ab6b77e1eb7925f8c8ab3d9dd72c3058089
samrkt12 <samrkt12@gmail.com>
2023-08-27 07:45:16 +0530

    edit theme file

--- src/context/theme-context.js
+++ src/context/theme-context.js
@@ -5,14 +5,7 @@ import { createContext, useEffect, useState } from "react";
 export const ThemeContext = createContext();
 
 export const ThemeContextProvider = ({ children }) => {
-  const [mode, setMode] = useState(() => {
-    const initialTheme = localStorage.getItem("theme-myblog");
-    return initialTheme ? initialTheme : "dark";
-  });
-
-  useEffect(() => {
-    localStorage.setItem("theme-myblog", mode);
-  }, [mode]);
+  const [mode, setMode] = useState("dark");
 
   const toggle = () => {
     setMode((prev) => (prev === "dark" ? "light" : "dark"));
