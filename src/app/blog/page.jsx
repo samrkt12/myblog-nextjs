2da673d94fe57fa94259467ebff26318a7b8bd7b
samrkt12 <samrkt12@gmail.com>
2023-08-27 08:36:11 +0530

    localhost

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import styles from "./blog.module.css";
-import BlogList from "@/components/BlogList.jsx/BlogList";
-
+// import BlogList from "@/components/BlogList.jsx/BlogList";
+import Link from "next/link";
+import Image from "next/image";
 async function getData() {
   try {
     const res = await fetch(`${process.env.NEXTAUTH_URL}/api/blogs`, {
@@ -14,13 +15,39 @@ async function getData() {
     return res.json();
   } catch (error) {
     console.error("Error fetching data:", error);
-    throw error;
+    // throw error;
   }
 }
 
 const Blog = async () => {
   const data = await getData();
-  return <BlogList styles={styles} data={data} />;
+  return (
+    <div className={styles.mainContainer}>
+      {data &&
+        data.map((item) => (
+          <Link
+            href={`/blog/${item._id}`}
+            className={styles.container}
+            key={item._id}
+          >
+            <div className={styles.imgContainer}>
+              <Image
+                src={item.image}
+                alt="blog-image"
+                width={400}
+                height={250}
+                className={styles.image}
+              />
+            </div>
+            <div className={styles.content}>
+              <h1 className={styles.title}>{item.title}</h1>
+              <p className={styles.desc}>{item.description}</p>
+            </div>
+          </Link>
+        ))}
+    </div>
+  );
+  // return <BlogList styles={styles} data={data} />;
 };
 
 export default Blog;
--- src/components/BlogList.jsx/BlogList.jsx
+++ src/components/BlogList.jsx/BlogList.jsx
@@ -1,32 +1,7 @@
 import React from "react";
-import Link from "next/link";
-import Image from "next/image";
+
 const BlogList = ({ styles, data }) => {
-  return (
-    <div className={styles.mainContainer}>
-      {data.map((item) => (
-        <Link
-          href={`/blog/${item._id}`}
-          className={styles.container}
-          key={item._id}
-        >
-          <div className={styles.imgContainer}>
-            <Image
-              src={item.image}
-              alt="blog-image"
-              width={400}
-              height={250}
-              className={styles.image}
-            />
-          </div>
-          <div className={styles.content}>
-            <h1 className={styles.title}>{item.title}</h1>
-            <p className={styles.desc}>{item.description}</p>
-          </div>
-        </Link>
-      ))}
-    </div>
-  );
+  return <></>;
 };
 
 export default BlogList;
